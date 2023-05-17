c9234cc39992b0699ddfcc12c6fbe842d17c91c3
Ira S <iryna.shevchenko2@gmail.com>
2023-05-18 00:09:49 +0300

    Initial settings done

---
+++ src/main.js
@@ -0,0 +1,67 @@
+
+// Mobile menu
+
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
+
+
+
+// Thank-you pop-up
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-thank-you-open]"),
+    closeModalBtn: document.querySelector("[data-modal-thank-you-close]"),
+    modal: document.querySelector("[data-modal-thank-you]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+
+// Your-order pop-up
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-your-order-open]"),
+    closeModalBtn: document.querySelector("[data-modal-your-order-close]"),
+    modal: document.querySelector("[data-modal-your-order]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
